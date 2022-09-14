7216212f82e661a4a63b8d0371953e900bc3b341
CarlZeidler <carl.zeidler1@gmail.com>
2022-09-14 14:46:42 +0200

    Update Assignment1.cs

--- Assets/Assignment1.cs
+++ Assets/Assignment1.cs
@@ -16,6 +16,8 @@ public class Assignment1 : ProcessingLite.GP21
         //StrokeWeight(0.5f);
         //Stroke(230, 0, 0, 85);
 
+        //2. Namn
+
         ////E
         //Line(1, 3, 3, 3);
         //Line(1, 3, 1, 7);
@@ -57,110 +59,68 @@ public class Assignment1 : ProcessingLite.GP21
         //Line(16.5f, 3, 15.5f, 3.5f);
 
 
-        ////shapes
+        ////3. Abstract shapes
         //Stroke(0, 0, 0);
 
-        //Fill(255, 165, 0);
+        //Fill(Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
         //Triangle(0, 0, 1, 1, 2, 0);
 
-        //Fill(255, 165, 0);
-        //Triangle(2, 0, 3, 1, 4, 0);
+        //Fill(Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
+        //Rect(2, 3, 3, 1);
+
+        //Fill(Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
+        //Circle(4, 1.2f, 1.5f);
+
+        //Fill(Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
+        //Square(9, 5, 0.5f);
+
+        ////4. & 5. Parabolic loop
+        //float x1 = 1f;
+        //float y1 = 1f;
+        //float x2 = 1f;
+        //float y2 = 9f;
+
+        //StrokeWeight(0.2f);
+
+        //for (int i = 0; i < 33; i++)
+        //{
+        //    if (i % 3 == 0)
+        //    {
+        //        Stroke(Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
+        //    }
+        //    else 
+        //    {
+        //        Stroke(255);
+        //    }
 
 
-        //Parabolic loop
+        //    if (i < 8)
+        //    {
+        //        Line(x1, y1, x2, y2);
+        //        y1++;
+        //        x2++;
+        //    }
+        //    else if (i < 16)
+        //    {
+        //        Line(x1, y1, x2, y2);
+        //        x1++;
+        //        y2--;
+        //    }
+        //    else if (i < 24)
+        //    {
+        //        Line(x1, y1, x2, y2);
+        //        y1--;
+        //        x2--;
+        //    }
+        //    else if (i < 33)
+        //    {
+        //        Line(x1, y1, x2, y2);
+        //        x1--;
+        //        y2++;
+        //    }
 
 
-
-        ////First quadrant
-        //Line(1, 1, 1, 9);
-        //Line(1, 2, 2, 9);
-        //Line(1, 3, 3, 9);
-        //Line(1, 4, 4, 9);
-        //Line(1, 5, 5, 9);
-        //Line(1, 6, 6, 9);
-        //Line(1, 7, 7, 9);
-        //Line(1, 8, 8, 9);
-        //Line(1, 9, 9, 9);
-
-        ////Second quadrant
-        //Line(1, 9, 9, 9);
-        //Line(2, 9, 9, 8);
-        //Line(3, 9, 9, 7);
-        //Line(4, 9, 9, 6);
-        //Line(5, 9, 9, 5);
-        //Line(6, 9, 9, 4);
-        //Line(7, 9, 9, 3);
-        //Line(8, 9, 9, 2);
-        //Line(9, 9, 9, 1);
-
-        ////Third quadrant
-        //Line(9, 9, 9, 1);
-        //Line(9, 8, 8, 1);
-        //Line(9, 7, 7, 1);
-        //Line(9, 6, 6, 1);
-        //Line(9, 5, 5, 1);
-        //Line(9, 4, 4, 1);
-        //Line(9, 3, 3, 1);
-        //Line(9, 2, 2, 1);
-        //Line(9, 1, 1, 1);
-
-        ////Fourth quadrant
-        //Line(9, 1, 1, 1);
-        //Line(8, 1, 1, 2);
-        //Line(7, 1, 1, 3);
-        //Line(6, 1, 1, 4);
-        //Line(5, 1, 1, 5);
-        //Line(4, 1, 1, 6);
-        //Line(3, 1, 1, 7);
-        //Line(2, 1, 1, 8);
-        //Line(1, 1, 1, 9);
-
-        float x1 = 1f;
-        float y1 = 1f;
-        float x2 = 1f;
-        float y2 = 9f;
-
-        StrokeWeight(0.2f);
-
-        for (int i = 0; i < 33; i++)
-        {
-            if (i % 3 == 0)
-            {
-                Stroke(Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
-            }
-            else 
-            {
-                Stroke(255);
-            }
-
-
-            if (i < 8)
-            {
-                Line(x1, y1, x2, y2);
-                y1++;
-                x2++;
-            }
-            else if (i < 16)
-            {
-                Line(x1, y1, x2, y2);
-                x1++;
-                y2--;
-            }
-            else if (i < 24)
-            {
-                Line(x1, y1, x2, y2);
-                y1--;
-                x2--;
-            }
-            else if (i < 33)
-            {
-                Line(x1, y1, x2, y2);
-                x1--;
-                y2++;
-            }
-
-
-        }
+        //}
 
     }
 
